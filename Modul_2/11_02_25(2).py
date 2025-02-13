ceb8bdce4973dcc7e10578adb2238cf6a928efb1
nahiraaa <nurainistudy08@gmail.com>
2025-02-13 09:22:15 +0700

    Add files via upload

---
+++ Modul_2/11_02_25(2).py
@@ -0,0 +1,13 @@
+Angka_pertama = int(input("Masukkan Angka pertama: "))
+Angka_kedua = int(input("Masukkan Angka kedua: "))
+
+# Change 'angka1' and 'angka2' to 'Angka_pertama' and 'Angka_kedua'
+jumlah = Angka_pertama + Angka_kedua
+kurang = Angka_pertama - Angka_kedua
+kali = Angka_pertama * Angka_kedua
+bagi = Angka_pertama / Angka_kedua
+
+print("Hasil penjumlahan:", jumlah)
+print("Hasil pengurangan:", kurang)
+print("Hasil perkalian:", kali)
+print("Hasil pembagian:", bagi)
---
+++ Modul_2/11_02_25.py
@@ -0,0 +1,4 @@
+Angka_pertama = int(input("Masukkan Angka pertama: "))
+Angka_kedua = int(input("Masukkan Angka kedua: "))
+jumlah = Angka_pertama + Angka_kedua
+print ("Hasil: ",jumlah)
